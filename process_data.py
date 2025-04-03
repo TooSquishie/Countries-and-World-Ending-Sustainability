ea5faa402b412dcec1e976fd7e91f243b6129f0a
Derek H <77030893+TooSquishie@users.noreply.github.com>
2025-04-02 16:29:04 -0800

    Added intial data_processing.py
    Simply read the csv and print some information

---
+++ process_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+globe_sustain = pd.read_csv("C:\\Users\\defen\\OneDrive\\Documents\\PythonStuff\\DataMiningClass\\Assn2\\Data\\global-data-on-sustainable-energy.csv")
+
+print(globe_sustain.info())
+print(globe_sustain.head())
+
+# For handling null values
+# https://www.youtube.com/watch?v=9ARreeMweNM
+
