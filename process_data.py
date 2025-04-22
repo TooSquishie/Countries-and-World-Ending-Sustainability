77d1700665d5e1e25a6633bc56c09e45fe80b19e
Derek H <77030893+TooSquishie@users.noreply.github.com>
2025-04-22 00:50:39 -0800

    sync progress process_data.py

--- process_data.py
+++ process_data.py
@@ -2,16 +2,17 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-globe_sustain = pd.read_csv("C:\\Users\\defen\\OneDrive\\Documents\\PythonStuff\\DataMiningClass\\Assn2\\Data\\global-data-on-sustainable-energy.csv")
+globe_sustain = pd.read_csv("C:\\Users\\defen\\OneDrive\\Documents\\PythonStuff\\DataMiningClass\\Assn2\\Data\\global-data-on-sustainable-energy_renamed.csv")
 
 print(globe_sustain.info())
 print(globe_sustain.head())
-print(globe_sustain['Density\n(P/Km2)'])
+print(globe_sustain['pop_density_square_km'].isnull().sum())
+
 # Set the target variable
-target = globe_sustain['Value_co2_emissions_kt_by_country']
+target = globe_sustain['carbon_emit_m_tons']
 
 # Set the feature variables
-features = globe_sustain.drop('Value_co2_emissions_kt_by_country', axis=1)
+features = globe_sustain.drop('carbon_emit_m_tons', axis=1)
 
 # For handling null values
 # https://www.youtube.com/watch?v=9ARreeMweNM
@@ -26,11 +27,15 @@ plt.ylabel('Number of Nulls')
 plt.tight_layout()  # Optional: Adjusts layout to prevent label cut-off
 plt.show()
 
+testdf = globe_sustain.drop(columns=['country'],axis=1, inplace=False).copy() 
+
+print(testdf.info())
+
 # Assuming df is your DataFrame and 'target' is your target variable
-correlation_matrix = globe_sustain.corr(method='pearson') # or 'spearman'
+correlation_matrix = testdf.corr(method='pearson') # or 'spearman'
 
 # Print correlations with the target variable
-print(correlation_matrix['Value_co2_emissions_kt_by_country'].sort_values(ascending=False))
+print(correlation_matrix['carbon_emit_m_tons'].sort_values(ascending=False))
 
 plt.figure(figsize=(10, 8))
 sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
